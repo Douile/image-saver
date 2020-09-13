5c1c5de21a0994c6e74b3f044173fa8c7d8b15b8
Douile <25043847+Douile@users.noreply.github.com>
2020-09-14 00:27:22 +0100

    Finish save button for devtools panel

--- devtools-sidebar.js
+++ devtools-sidebar.js
@@ -1,6 +1,10 @@
 'use strict';
 
-console.log(window);
+let activeImages = {
+  src: null,
+  background: null,
+  svg: null
+};
 
 function createElementsObject() {
   let cache = {};
@@ -37,16 +41,28 @@ function inspectedEval(evalString, defaultValue) {
 }
 
 async function evaluateActive() {
-  return {
+  let res = {
     tagName: await inspectedEval('$0.tagName', null),
     src: await inspectedEval('$0.getAttribute("src")', null),
     background: await inspectedEval('getComputedStyle($0).getPropertyValue("background-image")', null)
+  };
+  if (res.background !== null) {
+    if (res.background === 'none') {
+      res.background = null;
+    } else {
+      const match = res.background.match(/url\(["']?([^)"']+)["']?\)/);
+      if (match !== null) {
+        res.background = match[1];
+      }
+    }
   }
+
+  return res;
 }
 
 async function updateImages() {
   const activeElement = await evaluateActive();
-  console.log(activeElement);
+
   Elements['.image-src input'].value = activeElement.src;
   Elements['.image-src img'].src = activeElement.src;
   Elements['.image-background input'].value = activeElement.background;
@@ -55,11 +71,18 @@ async function updateImages() {
   URL.revokeObjectURL(Elements['.image-svg img'].src);
   Elements['.image-svg img'].src = '';
 
+  let svgContent = null;
   if (activeElement.tagName === 'svg') {
-    const svgContent = (await cleanSVG(await inspectedEval('$0.outerHTML'))).text;
+    svgContent = (await cleanSVG(await inspectedEval('$0.outerHTML'))).text;
     Elements['.image-svg input'].value = svgContent;
     Elements['.image-svg img'].src = URL.createObjectURL(new Blob([svgContent], { type: 'image/svg+xml' }));
   }
+
+  activeImages = {
+    src: activeElement.src,
+    background: activeElement.background,
+    svg: svgContent
+  };
 }
 
 updateTheme();
@@ -67,8 +90,25 @@ updateImages().then(null, console.error);
 
 browser.devtools.panels.onThemeChanged.addListener(function() {
   updateTheme();
-})
+});
 
 browser.devtools.panels.elements.onSelectionChanged.addListener(function() {
   updateImages().then(null, console.error);
+});
+
+window.addEventListener('click', function(e) {
+  if (!e.target) return;
+  for (let className of e.target.classList) {
+    switch (className) {
+      case 'image-src--save':
+      browser.runtime.sendMessage({ type: 'download', target: 'url', data: activeImages.src });
+      break;
+      case 'image-background--save':
+      browser.runtime.sendMessage({ type: 'download', target: 'url', data: activeImages.background });
+      break;
+      case 'image-svg--save':
+      browser.runtime.sendMessage({ type: 'download', target: 'base64', data: btoa(activeImages.svg) });
+      break;
+    }
+  }
 })
